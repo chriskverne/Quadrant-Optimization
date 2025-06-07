700d184773413be21523bbdd67f56074a24da84e
Christopher Kverne <chris.kverne@gmail.com>
2025-06-07 10:40:45 -0400

    finished implementing SPSA to compare it with freezing algorithm

---
+++ parameter_freezing/baseline_spsa.py
@@ -0,0 +1,75 @@
+import pennylane as qml
+import pennylane.numpy as pnp
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import math
+from tqdm import tqdm
+from helper.fetch_mnist import fetch_mnist, preprocess_image
+from helper.create_qnn_no_noise import create_qnn
+from helper.cross_entropy import cross_entropy_loss
+from data.params import *
+import pandas as pd
+
+def train_qnn_param_shift(x, y, n_qubits, n_layers, num_measurment_gates, num_epochs):
+    forward_pass = create_qnn(n_layers, n_qubits)
+    fp = 0
+    params = two_four
+    loss_history = []
+    fp_history = []
+
+    opt = qml.SPSAOptimizer(A=0.1) # qml.AdamOptimizer(stepsize=0.001, beta2=0.999)
+
+    """Training Loop"""
+    for time_step in tqdm(range(num_epochs), desc="Time step"):
+        s = 50
+        x_t = x[time_step*s:(time_step+1)*s]
+        y_t = y[time_step*s:(time_step+1)*s]
+        epoch_loss = 0
+        correct_predictions = 0
+        
+        for image, label in tqdm(zip(x_t, y_t), total=len(x_t), desc=f"Epoch {time_step+1}/{num_epochs}", leave=False):
+            def cost_fn(params):
+                out = forward_pass(image, params, num_measurment_gates)
+                return cross_entropy_loss(out, label)
+            
+            # Compute loss with current parameters
+            out = forward_pass(image, params, num_measurment_gates)
+            #fp+=1
+            loss = cross_entropy_loss(out, label)
+            epoch_loss += loss
+            
+            # Check if prediction is correct
+            pred = pnp.argmax(out)
+            if pred == label:
+                correct_predictions += 1
+
+            # increase fp by 2
+            fp += 2#*params.size 
+
+            # Update active params only
+            params = opt.step(cost_fn, params)
+
+        # Calculate average loss and accuracy
+        avg_loss = epoch_loss / len(x_t)
+        accuracy = correct_predictions / len(x_t)
+        loss_history.append(avg_loss)
+        fp_history.append(fp)
+        print(f"\nNo FP: {fp}, Epoch {time_step+1}/{num_epochs}, Avg Loss: {avg_loss:.4f}, Accuracy: {accuracy:.2%}")
+
+    return params, loss_history
+
+    
+# --------------------------------- Model Setup ---------------------------
+df = pd.read_csv('../data/two_digit.csv')
+x = df.drop('label', axis=1).values
+y = df['label'].values
+
+num_qubits = num_components = 4
+num_layers = 2
+num_measurment_gates = 1
+num_epochs = 300
+x = preprocess_image(x, num_components)
+
+
+train_qnn_param_shift(x, y, num_qubits, num_layers, num_measurment_gates, num_epochs)
