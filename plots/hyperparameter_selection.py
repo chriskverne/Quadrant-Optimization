1b61e234629a789708869c6cb9121c093a926c70
Christopher Kverne <chris.kverne@gmail.com>
2025-07-17 01:26:58 -0400

    working on hyperparemeter plots

--- plots/hyperparameter_selection.py
+++ plots/hyperparameter_selection.py
@@ -74,23 +74,30 @@ ty4 = [1.58859, 1.2752, 1.0989, 1.0604, 1.0312, 0.9896, 0.9837, 0.9500, 0.9329]
 plt.figure(figsize=(10, 6))
 
 # Freeze thresholds
-# plt.plot(x,y,label='s=100, F=0%')
-# plt.plot(x1,y1,label='s=100, F=10%')
-# plt.plot(x2,y2,label='s=100, F=30%')
-# plt.plot(x3,y3,label='s=100, F=50%')
-# plt.plot(x4,y4,label='s=100, F=70%')
-# plt.plot(x5,y5,label='s=100, F=90%')
+plt.plot(x,y,label='s=100, F=0%', linewidth=3)
+plt.plot(x1,y1,label='s=100, F=10%', linewidth=3)
+plt.plot(x2,y2,label='s=100, F=30%', linewidth=3)
+plt.plot(x3,y3,label='s=100, F=50%', linewidth=3)
+plt.plot(x4,y4,label='s=100, F=70%', linewidth=3, marker='D', markersize=8)
+plt.plot(x5,y5,label='s=100, F=90%', linewidth=3)
+
+styles = ['-', '-', '-', '-']
+markers = ['', '', '', '']
 
 # Timestep thresholds
-plt.plot(tx,ty,label='s=25, F=70%')
-plt.plot(tx2,ty2,label='s=100, F=70%')
-plt.plot(tx3,ty3,label='s=300, F=70%')
-plt.plot(tx4,ty4,label='s=500, F=70%')
+# plt.plot(tx,ty,label='s=25,   F=70%', linewidth=3, linestyle='-')
+# plt.plot(tx2,ty2,label='s=100, F=70%', linewidth=3, linestyle='-', marker='D', markersize=8)
+# plt.plot(tx3,ty3,label='s=300, F=70%', linewidth=3, linestyle='-')
+# plt.plot(tx4,ty4,label='s=500, F=70%', linewidth=3, linestyle='-')
 
-plt.title('Loss Decrease Over Forward Passes')
-plt.xlabel('Number of Forward Passes')
-plt.ylabel('Loss')
-plt.legend()
-plt.grid(True)
+#plt.title('Loss Decrease Over Forward Passes')
+plt.xlabel('Number of Forward Passes', fontsize='13')
+plt.ylabel('Cross Entropy Loss', fontsize='13')
+plt.tick_params(axis='both', which='major', labelsize=12)
+plt.legend(loc='upper right', framealpha=1.0, facecolor='white', edgecolor='black', fontsize=14)
+plt.minorticks_on()
+plt.grid(True, which='major', alpha=0.5, linestyle='-', linewidth=0.8, color='gray')
+plt.grid(True, which='minor', alpha=0.3, linestyle=':', linewidth=0.5, color='gray')
+plt.gca().set_facecolor("#CDD6D9FF")
 plt.xlim(0,300000)
 plt.show()
